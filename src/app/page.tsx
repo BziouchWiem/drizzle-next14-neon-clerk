c5b51ac43760488c35adfe525de2b004270062aa
Wiem Bziouich <bziouichwiem@gmail.com>
2025-07-27 12:24:12 +0100

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
 import { getData } from "@/actions/todoActions";
 import { getAllUsers, getUser } from "@/actions/userActions";
-//import Todos from "@/components/Todos";
+import Todos from "@/components/Todos";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 
 export default async function Home() {
-  const users = await getAllUsers();
-  console.log(users);
+  const user: any = await currentUser();
+  if (!user) return;
+  const fetchedData = await getUser(user?.id);
+  console.log(fetchedData);
 
-/*   const data = await getData(users[0].id); // todos du premier utilisateur
-  const user = await getUser(users[0].id);
-  console.log(user) */
-
- // return (
-   // <main className="flex items-center justify-between">
-    //  <Todos todos={data} user={users[0]} /> {/* ici user est "1" en dur */}
-    //</main>
-  //);
+  return (
+    fetchedData && (
+      <main className="flex  items-center justify-between ">
+        <Todos todos={fetchedData[0].todos} user={fetchedData[0]} />
+      </main>
+    )
+  );
 }
